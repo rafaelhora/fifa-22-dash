b39a30811044ca09a8093fff3c314f731aea0a54
Rafael da Hora Fraga <f.rafaelhora@gmail.com>
2021-10-26 18:36:06 -0300

    add potential finder
    Added player finder by potential with filters, pending formatting

--- dashboard.py
+++ dashboard.py
@@ -3,6 +3,9 @@ import numpy as np
 import streamlit as st
 import matplotlib.pyplot as plt
 import seaborn as sns
+import chart_studio.plotly as py
+import cufflinks as cf
+import plotly.express as px
 
 st.set_page_config(layout="wide")
 
@@ -10,9 +13,17 @@ container_intro = st.container()
 with container_intro:
     st.title('FIFA 22 Companion Dashboard')
     st.subheader('This is an companion app containing all players from the FIFA 22 roster. This can help you find players for your career mode, and choose with which team to play.')
-    st.warning('Caution, showing the raw dataset can take minutes.')
+    st.warning('Caution, showing the unfiltered raw dataset can take minutes.')
 
 
+## Page Elements
+def options_sidebar():
+    st.sidebar.markdown('# Options')
+
+
+# sidebar
+st.sidebar.title('Options sidebar')
+st.sidebar.button('Click me')
 
 ## Helpers
 
@@ -33,6 +44,17 @@ def get_data():
 
     return df, df_extra_info
 
+#show raw dataset
+def show_raw_data():
+    if st.sidebar.checkbox('Show Raw Data') == True:
+        st.dataframe(df)
+    else:
+        return
+
+df, df_extra = get_data()
+data = show_raw_data()
+
+
 # total value of club roster
 def club_value():
     values = df.groupby('Club', as_index=False).sum()[['Club','Value']].reset_index()
@@ -47,36 +69,47 @@ def club_wage():
     wages.set_index('Club', inplace=True)
     return wages
 
-def show_raw_data():
-    if container_intro.checkbox('Show Raw Data') == True:
-        st.dataframe(df)
-    else:
-        return
+#show most promising players
+def most_promising(wage_range, value_range, age_range, current_rating_range, potential_range,player_position, top_n):
+    promising_players = df[['ID', 'Name', 'Age','Club','Best Position','Best Overall Rating', 'Potential','Value', 'Wage']]
+    promising_players['future_growth'] = promising_players['Potential'] - promising_players['Best Overall Rating']
+    promising_players = promising_players.sort_values(['future_growth', 'Potential'], ascending = False)
+    promising_players = promising_players[promising_players['Wage'].between(wage_range[0], wage_range[1]) & promising_players['Value'].between(value_range[0], value_range[1])  & promising_players['Age'].between(age_range[0], age_range[1]) & promising_players['Best Overall Rating'].between(current_rating_range[0], current_rating_range[1]) & promising_players['Potential'].between(potential_range[0], potential_range[1])]
+    promising_players = promising_players.iloc[:int(top_n)]
+    return promising_players
 
-df, df_extra = get_data()
-data = show_raw_data()
+
+
+
 
 ## Plots
 
+#top 10 roster value
+#club_value_fig = px.bar(club_value(), y = 'Value')
+#st.plotly_chart(club_value_fig)
 
+#top 10 total wages
+#club_wage_fig = px.bar(club_wage(), y='Wage')
+#st.plotly_chart(club_wage_fig)
 
-
-## Page Elements
-def options_sidebar(options):
-    st.sidebar.markdown('# Options')
-
-
-# sidebar
-st.sidebar.title('Options sidebar')
-st.sidebar.button('Click me')
-
+#most promising players
+'''
+## Most promising Players
+'''
 #columns
 col1, col2 = st.columns(2)
-
-
-#Club information
 with col1:
-    st.bar_chart(club_value())
-
+    wage_range = st.slider("Wage Range", min(df.Wage), max(df.Wage),(min(df.Wage), max(df.Wage)), step = 500.0)
+    value_range = st.slider("Value Range", min(df.Value), max(df.Value),(min(df.Value), max(df.Value)), step = 50000.0)
+    age_range = st.slider("Age Range", min(df.Age), max(df.Age),(min(df.Age), max(df.Age)))
 with col2:
-    st.bar_chart(club_wage())
+    current_rating_range = st.slider("Current rating", min(df['Best Overall Rating']), max(df['Best Overall Rating']), (min(df['Best Overall Rating']), max(df['Best Overall Rating'])))
+    potential_range = st.slider("Potential", min(df['Potential']), max(df['Potential']), (min(df['Potential']), max(df['Potential'])))
+    top_n = st.number_input("Display limit", min_value = 1, max_value = len(df.index), value = 20)
+player_position = st.multiselect("Player Best Postition", options = df['Best Position'].unique(), default = df['Best Position'].unique())
+
+#attributes = ['Age', 'Wage', 'Value', 'Current Rating', 'Potential', 'Best Position']
+#order_promising_players = st.multiselect("Order By", attributes)
+
+
+st.dataframe(most_promising(wage_range, value_range, age_range, current_rating_range, potential_range,player_position, top_n))
